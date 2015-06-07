c86ac521628ef0323d96b86f040052002a800817
Shino1024 <justincompspec@gmail.com>
2015-06-07 22:57:47 +0200

    Began working on the Python script, which is responsible for communicating with the chip and plotting the graph.

---
+++ serialscript/__main__.py
@@ -0,0 +1,20 @@
+'''
+The Python script that is responsible for downloading
+the data from the Arduino Mega2560 board and processing it.
+Skrypt w Pythonie, ktory sluzy do pobierania danych
+z mikrokontrolera i przetwarzania ich.
+'''
+
+'''
+Importing the modules:
+serial - for the serial communication.
+numpy - for processing the data.
+matplotlib - for plotting the graphs from the data.
+Importowanie modulow:
+serial - do komunikacji szeregowej.
+numpy - do analizy danych.
+matplotlib - do rysowania wykresow z danych.
+'''
+import serial
+import numpy
+import matplotlib
