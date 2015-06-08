a3fc4ff8d369973d864515af1bf251e6f5ceaf16
Shino1024 <justincompspec@gmail.com>
2015-06-08 22:42:27 +0200

    Something...

--- serialscript/__main__.py
+++ serialscript/__main__.py
@@ -10,11 +10,20 @@ Importing the modules:
 serial - for the serial communication.
 numpy - for processing the data.
 matplotlib - for plotting the graphs from the data.
+tkinter - for creating the graphical user interface.
 Importowanie modulow:
 serial - do komunikacji szeregowej.
 numpy - do analizy danych.
 matplotlib - do rysowania wykresow z danych.
+tkinter - do tworzenia graficznego interfejsu uzytkownika.
 '''
 import serial
 import numpy
 import matplotlib
+import tkinter
+
+'''
+Creating a serial object for communication with Arduino.
+Tworzenie obiektu do komunikacji szeregowej z Arduino.
+'''
+arduino = serial.Serial("/dev/tty.usbserial", 9600)
