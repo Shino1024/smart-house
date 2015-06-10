3afd9f0dbf0c7a39afa9cab947b8ddbe3b2b8a78
Shino1024 <justincompspec@gmail.com>
2015-06-10 22:38:26 +0200

    The script is divided into 2 files.

--- serialscript/__main__.py
+++ serialscript/__main__.py
@@ -9,17 +9,14 @@ z mikrokontrolera i przetwarzania ich.
 Importing the modules:
 serial - for the serial communication.
 numpy - for processing the data.
-matplotlib - for plotting the graphs from the data.
 tkinter - for creating the graphical user interface.
 Importowanie modulow:
 serial - do komunikacji szeregowej.
 numpy - do analizy danych.
-matplotlib - do rysowania wykresow z danych.
 tkinter - do tworzenia graficznego interfejsu uzytkownika.
 '''
 import serial
 import numpy
-import matplotlib
 import tkinter
 
 '''
---
+++ visualizer/__main__.py
@@ -0,0 +1,15 @@
+'''
+This file creates the graphs, basing on the log.log file.
+Ten plik tworzy wykresy na podstawie danych z pliku log.log.
+'''
+
+'''
+The libraries:
+matplotlib - for creating the graphs.
+Tkinter - for the GUI.
+Biblioteki:
+matplotlib - do stworzenia wykresow.
+Tkinter - do wykonania graficznego interfejsu uzytkownika.
+'''
+import matplotlib as mpl
+import Tkinter as tk
